24cf111aa2069a088a29c8bc0a3db8e4da5ef3aa
shunail2029 <tiger1998nov27@gmail.com>
2021-01-20 14:30:46 +0900

    get child chain params from config file

--- x/secretdb/types/types.go
+++ x/secretdb/types/types.go
@@ -1,1 +1,26 @@
 package types
+
+import "errors"
+
+// Flags
+const (
+	FlagChildCount = "child-count"
+	FlagChildUrls  = "child-urls"
+)
+
+// child chain params
+var (
+	ChildCount int
+	ChildUrls  []string
+)
+
+// SetChildParams ...
+func SetChildParams(count int, urls []string) error {
+	if count != len(urls) {
+		return errors.New("child-count should be equal to length of child-urls")
+	}
+
+	ChildCount = count
+	ChildUrls = urls
+	return nil
+}
