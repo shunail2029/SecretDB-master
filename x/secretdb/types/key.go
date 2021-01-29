7acf0091cb37743e90bb0c829b7ac519a1d0a12e
shunail2029 <tiger1998nov27@gmail.com>
2021-01-29 17:18:49 +0900

    rename module

--- x/secretdb/types/key.go
+++ x/secretdb/types/key.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// ModuleName is the name of the module
-	ModuleName = "secretdb-master"
+	ModuleName = "secretdbMaster"
 
 	// StoreKey to be used when creating the KVStore
 	StoreKey = ModuleName
